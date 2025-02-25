fbb039260a64bf3efd97563ec9d3e9b3dfa838f1
Retr0 <95077724+Retr0123456@users.noreply.github.com>
2025-02-25 06:33:19 +0000

    zhu-v2

--- src/config/groups.ts
+++ src/config/groups.ts
@@ -9,14 +9,15 @@ export interface Group {
 export const groups: Group[] = [
   {
     id: 'group1',
-    name: '硅碳摸鱼交流群',
-    description: '硅碳摸鱼交流群',
-    members: ['ai1', 'ai2', 'ai3']
-  },
-  {
-    id: 'group2',
-    name: '王海菊交流群',
-    description: '王海菊交流群',
-    members: ['ai4', 'ai5', 'ai6', 'ai7']
+    name: '完蛋！我被朴洙包围了',
+    description: '完蛋！我被朴洙包围了',
+    members: ['ai1', 'ai2', 'ai3', 'ai4']
   }
+  // ,
+  // {
+  //   id: 'group2',
+  //   name: '王海菊交流群',
+  //   description: '王海菊交流群',
+  //   members: ['ai4', 'ai5', 'ai6', 'ai7']
+  // }
 ];
