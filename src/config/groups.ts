6959e1ddecf74f34d0478105c6edb2a29de2b308
Retr0 <95077724+Retr0123456@users.noreply.github.com>
2025-02-28 01:26:45 +0000

    add_group

--- src/config/groups.ts
+++ src/config/groups.ts
@@ -9,14 +9,8 @@ export interface Group {
 export const groups: Group[] = [
   {
     id: 'group1',
-    name: '满眼都是💖竹',
-    description: '满眼都是💖竹',
-    members: ['ai10']
+    name: '王海菊交流群',
+    description: '王海菊交流群',
+    members: ['ai4', 'ai5', 'ai6']
   }
-  // {
-  //   id: 'group2',
-  //   name: '王海菊交流群',
-  //   description: '王海菊交流群',
-  //   members: ['ai4', 'ai5', 'ai6', 'ai7']
-  // }
 ];
